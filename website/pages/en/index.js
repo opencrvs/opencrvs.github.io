b5f8ddde9cfad3684cb4c11fc2283936f9eb7a6d
Euan Millar <euanmillar77@gmail.com>
2020-05-26 08:57:09 +0100

    more homepage sections

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -84,7 +84,7 @@ class Index extends React.Component {
           <div className="openCrvsApp">
             <img src="img/openCrvsApp.png" alt="OpenCRVS Application" />
           </div>
-          <h2>
+          <h2 className="visionHeader">
             Our vision is to ensure that every individual on the planet is
             recognised, protected and provided for from birth.
           </h2>
@@ -99,21 +99,11 @@ class Index extends React.Component {
         background={props.background}
       >
         <GridBlock
-          align="center"
+          align="left"
           contents={props.children}
           layout={props.layout}
         />
       </Container>
-    );
-
-    const FeatureCallout = () => (
-      <div
-        className="productShowcaseSection paddingBottom"
-        style={{ textAlign: "center" }}
-      >
-        <h2>Feature Callout</h2>
-        <MarkdownBlock>These are features of this project</MarkdownBlock>
-      </div>
     );
 
     const TryOut = () => (
@@ -161,22 +151,374 @@ class Index extends React.Component {
     );
 
     const Features = () => (
-      <Block layout="fourColumn">
-        {[
-          {
-            content: "This is the content of my feature",
-            image: `${baseUrl}img/undraw_react.svg`,
-            imageAlign: "top",
-            title: "Feature One",
-          },
-          {
-            content: "The content of my second feature",
-            image: `${baseUrl}img/undraw_operating_system.svg`,
-            imageAlign: "top",
-            title: "Feature Two",
-          },
-        ]}
-      </Block>
+      <div className="productShowcaseSection container paddingBottom">
+        <div className="wrapper wrapperTitle">
+          <h2>A new model for civil registration</h2>
+          <div className="gridBlock bulletBlock">
+            <div className="blockElement threeByGridBlock">
+              <div className="blockContent">
+                <h4 className="bulletHeader">
+                  <div>
+                    <span>
+                      <p>Increases birth and death registration rates</p>
+                    </span>
+                  </div>
+                </h4>
+                <div>
+                  <span>
+                    <ul className="bullets">
+                      <li>
+                        Active registration at the community level to avoid
+                        prohibitive travel and costs
+                      </li>
+                      <li>
+                        Uses networks of trusted registration agents within the
+                        community
+                      </li>
+                      <li>
+                        Timely notification of births and deaths from hospitals
+                      </li>
+                    </ul>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="blockElement threeByGridBlock">
+              <div className="blockContent">
+                <h4 className="bulletHeader">
+                  <div>
+                    <span>
+                      <p>Makes civil registration easy</p>
+                    </span>
+                  </div>
+                </h4>
+                <div>
+                  <span>
+                    <ul className="bullets">
+                      <li>
+                        Simplified application process that includes reduced
+                        data collection requirements
+                      </li>
+                      <li>
+                        Customer-centric registration services, including
+                        application status updates
+                      </li>
+                      <li>Automated validation for accurate data collection</li>
+                    </ul>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="blockElement threeByGridBlock">
+              <div className="blockContent">
+                <h4 className="bulletHeader">
+                  <div>
+                    <span>
+                      <p>Drives efficiencies for government service delivery</p>
+                    </span>
+                  </div>
+                </h4>
+                <div>
+                  <span>
+                    <ul className="bullets">
+                      <li>
+                        Works with other e-government systems as a foundation
+                        for effective ID management
+                      </li>
+                      <li>
+                        Real-time operational data that supports increased
+                        accountability and performance management
+                      </li>
+                      <li>
+                        Digital first to reduce manual processes and paper
+                        archives
+                      </li>
+                    </ul>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+
+    const Process = () => (
+      <div className="productShowcaseSection container paddingBottom processSection">
+        <div className="wrapper wrapperTitle">
+          <h2>OpenCRVS process</h2>
+          <p>
+            While every country will have a slightly different process that uses
+            different actors, OpenCRVS supports key elements of a standardised
+            process that reflects best practice and drives efficiency.
+          </p>
+          <div className="gridBlock processBlock">
+            <div className="blockElement twoByGridBlock">
+              <div className="blockContent processContent">
+                <div className="iconHeader">
+                  <div>
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z"
+                        fill="#8049B7"
+                        fillOpacity="0.5"
+                      />
+                      <path
+                        d="M8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12Z"
+                        fill="#8049B7"
+                      />
+                    </svg>
+                  </div>
+                  <h3>Notify a vital event</h3>
+                </div>
+                <p>
+                  This is an optional step to collect a minimal set of vital
+                  event data that can be used for (i) planning and (ii)
+                  follow-up purposes. In OpenCRVS we call it an “in-progress”
+                  application because action is required to complete it. You can
+                  receive an “in-progress” application from the OpenCRVS
+                  application (as an incomplete application) as well as a number
+                  of external sources including:
+                </p>
+                <ul className="bullets">
+                  <li>
+                    <a href="#">Digital health systems</a> e.g. DHIS2 or OpenSRP
+                  </li>
+                  <li>
+                    Messaging apps e.g. SMS, USSD, Whatsapp etc.
+                    (self-notification)
+                  </li>
+                  <li>Refugee registration systems</li>
+                </ul>
+                <a className="homeLink" href="#">
+                  Learn more
+                </a>
+              </div>
+            </div>
+            <div className="blockElement twoByGridBlock">
+              <div className="blockContent processContent">
+                <div className="processImage">
+                  <img src="img/process1.png" alt="Notify a vital event" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="gridBlock processBlock">
+            <div className="blockElement twoByGridBlock">
+              <div className="blockContent processContent">
+                <div className="processImage">
+                  <img src="img/process2.png" alt="Declare a vital event" />
+                </div>
+              </div>
+            </div>
+            <div className="blockElement twoByGridBlock">
+              <div className="blockContent processContent">
+                <div className="iconHeader">
+                  <div>
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z"
+                        fill="#F1B162"
+                        fillOpacity="0.5"
+                      />
+                      <path
+                        d="M8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12Z"
+                        fill="#F1B162"
+                      />
+                    </svg>
+                  </div>
+                  <h3>Declare a vital event</h3>
+                </div>
+                <p>
+                  This is the completion of a vital event registration form. In
+                  OpenCRVS we call this an "application" as it is more familiar
+                  to service providers - all mandatory information + supporting
+                  documents required to register the vital event. This can be
+                  completed:
+                </p>
+                <ul className="bullets">
+                  <li>At the community level on a tablet or mobile device</li>
+                  <li>
+                    In an office on a PC, laptop or any other available device
+                  </li>
+                </ul>
+                <a className="homeLink" href="#">
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="gridBlock processBlock">
+            <div className="blockElement twoByGridBlock">
+              <div className="blockContent processContent">
+                <div className="iconHeader">
+                  <div>
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z"
+                        fill="#C1C7C9"
+                      />
+                      <path
+                        d="M8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12Z"
+                        fill="#707C80"
+                      />
+                    </svg>
+                  </div>
+                  <h3>Validate and register a vital event</h3>
+                </div>
+                <p>
+                  In OpenCRVS, this is ideally done in one step. In the “Ready
+                  to Review” workqueue, the Registrar (or Registration Agent)
+                  can find all applications sent from the field, review them
+                  alongside supporting documents, and then either reject or
+                  register them.
+                </p>
+                <a className="homeLink homeLinkNoBullets" href="#">
+                  Learn more
+                </a>
+              </div>
+            </div>
+            <div className="blockElement twoByGridBlock">
+              <div className="blockContent processContent">
+                <div className="processImage">
+                  <img
+                    src="img/process3.png"
+                    alt="Validate and register a vital event"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="gridBlock processBlock">
+            <div className="blockElement twoByGridBlock">
+              <div className="blockContent processContent">
+                <div className="processImage">
+                  <img src="img/process4.png" alt="Issue a certificate" />
+                </div>
+              </div>
+            </div>
+            <div className="blockElement twoByGridBlock">
+              <div className="blockContent processContent">
+                <div className="iconHeader">
+                  <div>
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z"
+                        fill="#49B78D"
+                        fillOpacity="0.5"
+                      />
+                      <path
+                        d="M8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12Z"
+                        fill="#49B78D"
+                      />
+                    </svg>
+                  </div>
+                  <h3>Issue a certificate</h3>
+                </div>
+                <p>
+                  Once the record has been registered, the certificate is “Ready
+                  to Print” and it appears in the workqueue of the same name,
+                  ready to be printed with digital signatures and collected. If
+                  digital signatures aren’t allowed it can be printed in advance
+                  and signatures added by hand.
+                </p>
+                <a className="homeLink homeLinkNoBullets" href="#">
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="gridBlock processBlock">
+            <div className="blockElement twoByGridBlock">
+              <div className="blockContent processContent">
+                <div className="iconHeader">
+                  <div>
+                    <svg
+                      width="21"
+                      height="20"
+                      viewBox="0 0 21 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M20 10H16L13 19L7 1L4 10H0"
+                        stroke="#4C68C1"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </div>
+                  <h3>Use civil registration data</h3>
+                </div>
+                <p>
+                  Civil registration data is incredibly valuable and OpenCRVS
+                  helps you realise this through:
+                </p>
+                <ul className="bullets">
+                  <li>
+                    <b>Vital statistics export:</b> export your civil
+                    registration data in a standard format that can be used in a
+                    vital statistics system
+                  </li>
+                </ul>
+                <ul className="bullets">
+                  <li>
+                    <b>Operational data:</b> this lets registration managers
+                    understand what is working and what is not and provide
+                    targeted support and resources
+                  </li>
+                </ul>
+                <ul className="bullets">
+                  <li>
+                    <b>Headline registration rates:</b> in real-time, see key
+                    rates required for ongoing performance management at the
+                    sub-national, national and global level, including SDG
+                    indicators that can be taken from civil registration data.
+                  </li>
+                </ul>
+                <a className="homeLink" href="#">
+                  Learn more
+                </a>
+              </div>
+            </div>
+            <div className="blockElement twoByGridBlock">
+              <div className="blockContent processContent">
+                <div className="processImage">
+                  <img
+                    src="img/process5.png"
+                    alt="Use civil registration data"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
 
     const Showcase = () => {
@@ -215,8 +557,7 @@ class Index extends React.Component {
         <div className="mainContainer">
           <OpenCRVSVision />
           <Features />
-          <FeatureCallout />
-          <LearnHow />
+          <Process />
           <TryOut />
           <Description />
           <Showcase />
